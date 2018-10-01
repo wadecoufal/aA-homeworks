9aff448269fe1f37cec08d70fdec8c6f75ce09fd
Wade Coufal <wade.coufal@gmail.com>
2018-10-01 09:03:16 -0700

    Add W7D1 Job Listing HW

--- W6D5/NPM_HW/webpack.config.js
+++ W6D5/NPM_HW/webpack.config.js
@@ -1,7 +1,56 @@
+// var path = require('path');
+//
+// module.exports = {
+//   entry: "./entry.jsx",
+//   output: {
+//       path: path.resolve(__dirname),
+//       filename: "bundle.js"
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: ['.js', '.jsx', '*']
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: [/\.jsx?$/],
+//         exclude: /(node_modules)/,
+//         use: {
+//           loader: 'babel-loader',
+//           query: {
+//             presets: ['@babel/env', '@babel/react']
+//           }
+//         },
+//       }
+//     ]
+//   }
+// };
+
+const path = require('path');
+
 module.exports = {
-  entry: "./frontend/my_app.jsx",
+  context: __dirname,
+  entry: "./entry.jsx",
   output: {
-      path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-      filename: "bundle.js"
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
   }
 };
