c0e749cc2b181c8f87d0235a9ca17bced51dcad0
Wade Coufal <wade.coufal@gmail.com>
2018-10-01 22:59:18 -0700

    Add W7D2 Redux HW

---
+++ W7D2/frontend/todo_redux.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+  store.dispatch = applyMiddlewares(store, addLoggingToDispatch);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+}
+
+const applyMiddlewares = function (store, ...middlewares) {
+  let dispatch = store.dispatch;
+
+  middlewares.forEach( (middleware) => {
+    dispatch = middleware(store)(dispatch);
+  })
+
+  Object.assign({}, store, { dispatch })
+}
